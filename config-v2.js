12a1597d7482b74b96ab171f38941dfb97dfc033
minu998 <slnoty56@gmail.com>
2024-07-27 22:54:06 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,6 +1,6 @@
 const GITHUB_AUTH_TOKEN = "null"
 const BOT_NUMBER = "null"
-const SESSION_ID = ""
+const SESSION_ID = "PRABATH-MD~VJ5yVaQK#wFzEDasCQQB3cCATkERXG6AyUKJWLo9aR6pEtPsGarU"
 
 
 module.exports = {
